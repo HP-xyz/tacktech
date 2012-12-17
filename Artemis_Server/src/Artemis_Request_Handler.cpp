1c4231d4c61b2e51e36dee394cbb38ffe69a649c
HP <HP@HP-PC>
2012-12-17 14:33:35 +0200

    Really prove that display_client_container exists

--- Artemis_Server/src/Artemis_Request_Handler.cpp
+++ Artemis_Server/src/Artemis_Request_Handler.cpp
@@ -314,6 +314,10 @@ void Artemis_Request_Handler::generate_queries(const std::string &request, boost
 #ifdef _SHOW_DEBUG_OUTPUT
 		std::cout << " - Received IDENTIFY command" << std::endl;
 		std::cout << "  - display_client_container.get() ---> " << display_client_container.get() << std::endl;
+		if (display_client_container.get() == 0)
+			std::cout << "  -> display_client_container == 0" << std::endl;
+		else
+			std::cout << "  -> display_client_container != 0" << std::endl;
 #endif // _SHOW_DEBUG_OUPTU
 		pugi::xml_node indentification_node = tacktech.child("Identity");
 		Display_Client_Ptr display_client(new Display_Client());
