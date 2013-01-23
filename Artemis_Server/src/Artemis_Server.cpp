119d2cc380932e2c8788ebfe8767dc573049912e
HP <HP@HP-PC.Home>
2013-01-23 12:34:10 +0200

    Trying again...

--- Artemis_Server/src/Artemis_Server.cpp
+++ Artemis_Server/src/Artemis_Server.cpp
@@ -36,15 +36,15 @@ namespace Artemis
 							std::cout << "  # Ping has been longer than " << parameters["alerts.warning_minutes"] << " minutes, sending WARNING" << std::endl;
 #endif // _SHOW_DEBUG_OUTPUT
 							std::string sendmail_command = "sendmail -t \"";
-							sendmail_command += "From: <DvorakUser@gmail.com>";
+							sendmail_command += "From: <DvorakUser@gmail.com>\n";
 							sendmail_command += "To: ";
-							sendmail_command += parameters["Tacktech.admin"];
-							sendmail_command += "Subject: Warning for " + it->get()->get_identification();
-							sendmail_command += "MIME-Version: 1.0";
-							sendmail_command += "Content-Type: text/plain";
+							sendmail_command += parameters["Tacktech.admin"] + "\n";
+							sendmail_command += "Subject: Warning for " + it->get()->get_identification() + "\n\n"; 
+							sendmail_command += "MIME-Version: 1.0\n";
+							sendmail_command += "Content-Type: text/plain\n";
 							sendmail_command += "The display '" + it->get()->get_identification() + "' has not pinged in " + parameters["alerts.warning_minutes"] + " minutes. ";
-							sendmail_command += "The last ping we recieved was " + boost::posix_time::to_iso_string((it->get()->get_last_ping() - boost::posix_time::second_clock::universal_time()).invert_sign()) + " ago.";
-							sendmail_command += "\n.\"";
+							sendmail_command += "The last ping we received was " + boost::posix_time::to_iso_string((it->get()->get_last_ping() - boost::posix_time::second_clock::universal_time()).invert_sign()) + " ago.";
+							sendmail_command += "\n\"\n";
 							system(sendmail_command.c_str());
 						}
 					}
@@ -54,15 +54,15 @@ namespace Artemis
 						std::cout << "  # Ping has been longer than " << parameters["alerts.critical_minutes"] << " minutes, sending CRITICAL" << std::endl;
 #endif // _SHOW_DEBUG_OUTPUT
 						std::string sendmail_command = "sendmail -t \"";
-						sendmail_command += "From: <DvorakUser@gmail.com>";
+						sendmail_command += "From: <DvorakUser@gmail.com>\n";
 						sendmail_command += "To: ";
-						sendmail_command += parameters["Tacktech.admin"];
-						sendmail_command += "Subject: CRITICAL warning for " + it->get()->get_identification();
-						sendmail_command += "MIME-Version: 1.0";
-						sendmail_command += "Content-Type: text/plain";
+						sendmail_command += parameters["Tacktech.admin"] + "\n";
+						sendmail_command += "Subject: CRITICAL warning for " + it->get()->get_identification() + "\n\n";
+						sendmail_command += "MIME-Version: 1.0\n";
+						sendmail_command += "Content-Type: text/plain\n";
 						sendmail_command += "The display '" + it->get()->get_identification() + "' has not pinged in " + parameters["alerts.critical_minutes"] + " minutes. ";
-						sendmail_command += "The last ping we recieved was " + boost::posix_time::to_iso_string((it->get()->get_last_ping() - boost::posix_time::second_clock::universal_time()).invert_sign()) + " ago.";
-						sendmail_command += "\n.\"";
+						sendmail_command += "The last ping we received was " + boost::posix_time::to_iso_string((it->get()->get_last_ping() - boost::posix_time::second_clock::universal_time()).invert_sign()) + " ago.";
+						sendmail_command += "\n\"\n";
 						system(sendmail_command.c_str());
 					}
 					
